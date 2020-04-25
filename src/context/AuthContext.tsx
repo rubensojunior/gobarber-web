bc931795638d9049ebbcb15dda05c7dfa6735e3c
rubensojunior <rubensojunior6@gmail.com>
2020-04-25 16:26:32 -0300

    implementing signin function with context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,11 @@
-import React, { createContext, useCallback } from 'react'
+/* eslint-disable @typescript-eslint/ban-types */
+import React, { createContext, useCallback, useState } from 'react'
 import api from '../services/api'
+
+interface AuthState {
+  token: string
+  user: Object
+}
 
 interface SingInCredentials {
   email: string
@@ -7,24 +13,40 @@ interface SingInCredentials {
 }
 
 interface AuthContextData {
-  name: string
+  user: Object
   signIn(credentials: SingInCredentials): Promise<void>
 }
 
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token')
+    const user = localStorage.getItem('@GoBarber:user')
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) }
+    }
+
+    return {} as AuthState
+  })
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     })
 
-    console.log(response.data)
+    const { token, user } = response.data
+
+    localStorage.setItem('@GoBarber:token', token)
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user))
+
+    setData({ token, user })
   }, [])
 
   return (
-    <AuthContext.Provider value={{ name: 'junior', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   )
