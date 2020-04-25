452d819eeb19937d1acd9b72c67d4f07f0957e1c
rubensojunior <rubensojunior6@gmail.com>
2020-04-25 12:40:19 -0300

    refactoring with components and create signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
 
 import logo from '../../assets/logo.svg'
 
@@ -8,13 +8,16 @@ import Input from '../../components/Input'
 
 import { Container, Content, Background } from './styles'
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logo} alt="GoBarber" />
 
       <form>
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
 
         <Input name="email" icon={FiMail} placeholder="E-mail" />
 
@@ -25,18 +28,15 @@ const SignIn: React.FC = () => (
           placeholder="Senha"
         />
 
-        <Button type="submit">Entrar</Button>
-
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
 
       <a href="create">
-        <FiLogIn />
-        Criar conta
+        <FiArrowLeft />
+        Voltar para logon
       </a>
     </Content>
-    <Background />
   </Container>
 )
 
-export default SignIn
+export default SignUp
