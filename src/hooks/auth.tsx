13a852ef143383fe8247c7a57b2a548d4d8a4447
rubensojunior <rubensojunior6@gmail.com>
2020-08-20 23:38:58 -0300

    creating auth context tests

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -89,10 +89,6 @@ const AuthProvider: React.FC = ({ children }) => {
 const useAuth = (): AuthContextData => {
   const context = useContext(AuthContext)
 
-  if (!context) {
-    throw new Error('useAuth must be used within a AuthProvider')
-  }
-
   return context
 }
 
